c6037eca2d9682cd438438a272bcaec04ee23bd0
Eldridge M. Mount IV <eldridge.mount@labxtechnologies.com>
2011-12-20 11:27:19 -0500

    Updated DTS and board file generation script

--- board/entexp/eeColor/gen_files.sh
+++ board/entexp/eeColor/gen_files.sh
@@ -3,4 +3,4 @@ echo 'Generating DTS file from SOPCINFO...'
 java -jar ../../../../tools/sopc2dts/sopc2dts.jar -t dts --board ./eeColor_BoardInfo.xml -i ./eecpb_sopc.sopcinfo -o ./eeColor.dts
 
 echo 'Generating U-Boot header file from SOPCINFO...'
-java -jar ../../../../tools/sopc2dts/sopc2dts.jar -t uboot -i ./eecpb_sopc.sopcinfo -o ./eeColor.h
+java -jar ../../../../tools/sopc2dts/sopc2dts.jar --type u-boot --input ./eecpb_sopc.sopcinfo --output ./eeColor.h
