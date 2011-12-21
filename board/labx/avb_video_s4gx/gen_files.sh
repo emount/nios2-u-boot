8285ff38a6c2dcdb09a157f8f0bc761c91ad4e96
Eldridge M. Mount IV <eldridge.mount@labxtechnologies.com>
2011-12-21 12:12:23 -0500

    Adding Lab X / AVB video board for Stratix-IV GX

---
+++ board/labx/avb_video_s4gx/gen_files.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo 'Generating DTS file from SOPCINFO...'
+java -jar ../../../../tools/sopc2dts/sopc2dts.jar -t dts --board ./avb_video_s4gx.xml -i ./avb_video_s4gx.sopcinfo -o ./avb_video_s4gx.dts
+
+echo 'Generating U-Boot header file from SOPCINFO...'
+java -jar ../../../../tools/sopc2dts/sopc2dts.jar --type u-boot --input ./avb_video_s4gx.sopcinfo --output ./avb_video_s4gx.h
