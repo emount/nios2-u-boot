dcf8985d5137a11772c9d2ae4efe5076cdfbed9a
emount <eldridge.mount@labxtechnologies.com>
2013-04-15 14:06:58 -0400

    Updated device tree, etc. for the Stratix-IV GX dev kit

--- board/labx/avb_video_s4gx/gen_files.sh
+++ board/labx/avb_video_s4gx/gen_files.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 echo 'Generating DTS file from SOPCINFO...'
-java -jar ../../../../nios2-tools/sopc2dts/sopc2dts.jar -t dts --board ./avb_video_s4gx.xml -i ./avb_video_s4gx.sopcinfo -o ./avb_video_s4gx.dts
+java -jar ../../../../nios2-tools/sopc2dts/sopc2dts.jar -t dts --pov-type cpu --pov nios2_qsys_0 --board ./avb_video_s4gx.xml -i ./avb_video_s4gx.sopcinfo --verbose -o ./avb_video_s4gx.dts
 
 echo 'Generating U-Boot header file from SOPCINFO...'
-java -jar ../../../../nios2-tools/sopc2dts/sopc2dts.jar --type u-boot --input ./avb_video_s4gx.sopcinfo --output ./avb_video_s4gx.h
+java -jar ../../../../nios2-tools/sopc2dts/sopc2dts.jar --type u-boot --pov-type cpu --pov nios2_qsys_0 --input ./avb_video_s4gx.sopcinfo --output ./avb_video_s4gx.h
+
+echo 'Generating environment script image for U-Boot...'
+../../../tools/mkimage -T script -C none -n "U-Boot Env Script" -d ./ub.config.scr ./ub.config.img
